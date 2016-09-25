eec8ac54f860c07fde7d21f4a740666693d5c42d
Oleg Voronin <Oleg.Voronin@vuw.ac.nz>
2016-09-25 16:33:49 +1300

    + mobile responsive

--- src/assets/toolkit/scripts/toolkit.js
+++ src/assets/toolkit/scripts/toolkit.js
@@ -1,3 +1,28 @@
 /**
  * Toolkit JavaScript
  */
+var Headroom = require('headroom.js');
+
+
+$(function(){
+	var $body = $('body');
+	var $globalNav = $("#global-nav");
+	if ($globalNav.length) {
+		var eGlobalNav = $globalNav[0];
+		var headroom  = new Headroom(eGlobalNav, {
+		  "offset": eGlobalNav.clientHeight,
+		  "tolerance": {
+		  	up: 20,
+		  	down: 5
+		  },
+		  onUnpin: function (){
+		  	$globalNav.toggleClass('is-open', false);
+		  }
+		});
+		headroom.init();
+
+		$body.on('click', '.js-toggle-global-nav', function(_event){
+			$globalNav.toggleClass('is-open');
+		});
+	}
+});
