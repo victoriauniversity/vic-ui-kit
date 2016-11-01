26ec9000bbd1d592dd9fe65221c5f8d464efe198
Joe Lobotka <ng.inornate@gmail.com>
2016-11-01 13:30:51 +1300

    [TEST]

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 
-if [ ${TRAVIS_BRANCH} = master ] ; then
-	# production
+# PRODUCTION build & deployment
+if [ $TRAVIS_BRANCH = 'master' ] ; then
+
   bash ./build-scripts/deploy-production.sh
   exit 0
 fi
 
 
+# DEVELOPMENT build & deployment
+if [ $TRAVIS_BRANCH = 'enhancements-jl-deployment' ] ; then
 
-if [ ${TRAVIS_BRANCH} = enhancements-jl-deployment ] ; then
-  # developmenet
   bash ./build-scripts/deploy-development.sh
   exit 0
 
 fi
 
-
+# NO build & deployment
 echo "Nothing deployable, ending CI build."
