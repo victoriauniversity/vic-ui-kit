5f0d2533e0ef65090be828500bdfda30c764e254
Joe Lobotka <ng.inornate@gmail.com>
2016-11-04 14:28:58 +1300

    [FIX] Rsync command, bumped toolkit.css.

--- build/deploy-prod.sh
+++ build/deploy-prod.sh
@@ -22,4 +22,4 @@ npm run release-prod
 rsync -avzP --exclude='[0-9]*' --delete --chmod=u=rwX,g=rX ./dist/ lobotkjo@rsync.keycdn.com:zones/static
 
 ## Syn everything inside the versioned directory EXCEPT meta data
-rsync rsync -avzP --delete --exclude={.*,*.md,*.json} --chmod=u=rwX,g=rX ./dist/[0-9]* lobotkjo@rsync.keycdn.com:zones/static
+rsync -avzP --delete --exclude={.*,*.md,*.json} --chmod=u=rwX,g=rX ./dist/[0-9]* lobotkjo@rsync.keycdn.com:zones/static
