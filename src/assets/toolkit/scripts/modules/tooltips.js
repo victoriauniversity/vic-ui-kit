606d60118eccba6dbac4a4c7a7b97890b8cbe0a4
Joe Lobotka <ng.inornate@gmail.com>
2019-02-08 14:49:05 +1300

    [HOTFIX] Adds polyfill for IE11 to support iterating through NodeLists.

--- src/assets/toolkit/scripts/modules/tooltips.js
+++ src/assets/toolkit/scripts/modules/tooltips.js
@@ -1,5 +1,17 @@
 import { isElementHidden, removeAttribute, isAppleMobileDevice } from '../utils/helpers';
 
+/** Library-specific polyfills */
+
+if ( 'NodeList' in window && !NodeList.prototype.forEach ) {
+  console.info( 'polyfill for IE11' );
+  NodeList.prototype.forEach = function (callback, thisArg) {
+    thisArg = thisArg || window;
+    for (var i = 0; i < this.length; i++) {
+      callback.call(thisArg, this[i], i, this);
+    }
+  };
+}
+
 
 // API interface
 const tooltipsApi = window.toolkitTooltips || {};
