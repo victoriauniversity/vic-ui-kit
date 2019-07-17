bd1ca09eccd3f213c6f7d9847661e5b250309322
Jake Ninness <jake.ninness@vuw.ac.nz>
2019-07-18 10:42:43 +1200

    Tweaks -- js only fire if tabs exist

--- src/assets/toolkit/scripts/modules/tabbed-search.js
+++ src/assets/toolkit/scripts/modules/tabbed-search.js
@@ -1,66 +1,73 @@
+$(() => {
+  // Only execute if tabs exist
+  if ( document.querySelectorAll( '#search-tab-js' ).length > 0 ) {
 
-const defaultActive = $('.p-search__tabs .active a').data('tab');
-const tabs = $('.p-search__tab a');
+    const defaultActive = $('.p-search__tabs .active a').data('tab');
+    const tabs = $('.p-search__tab a');
 
-console.log( defaultActive );
+    console.log( defaultActive );
 
-const $resultSections = $( '.search-results' );
+    const $resultSections = $( '.search-results' );
 
-console.log( $resultSections );
+    console.log( $resultSections );
 
-/* Sets default vault based on default active tab */
-$resultSections.each(( index, section ) => {
-  // element == this
-  const $section = $( section );
-  const sectionData = $section.data('content' );
-  console.log( $section.data('content' ));
+    /* Sets default vault based on default active tab */
+    $resultSections.each(( index, section ) => {
+      // element == this
+      const $section = $( section );
+      const sectionData = $section.data('content' );
+      console.log( $section.data('content' ));
 
-  // eslint-disable-next-line eqeqeq
-  if ( sectionData == defaultActive ) {
-    console.log( 'match ' + sectionData, defaultActive );
-    $section.addClass( 'search-active' );
-  } else {
-    $section.addClass( 'search-inactive' );
+      // eslint-disable-next-line eqeqeq
+      if ( sectionData == defaultActive ) {
+        console.log( 'match ' + sectionData, defaultActive );
+        $section.addClass( 'search-active' );
+      } else {
+        $section.addClass( 'search-inactive' );
+      }
+
+    });
+
+
+    //  tab click
+
+    //  not active change status set results set to show
+
+
+    console.log( 'tabs == ', tabs );
+
+    tabs.each(( _index, tab ) => {
+      // element == this
+      const $tab = $( tab );
+
+      $tab.on( 'click', () => {
+
+        // console.log( $tab );
+        if( !$tab.parent().hasClass('active')) {
+          // not active tab add class and remove from current
+          tabs.parents().removeClass( 'active' );
+          $tab.parent().addClass( 'active' );
+
+          // set matching content to show
+          console.log( $tab.data( 'tab' ));
+
+          // Match tab with content
+          const tabContent = $tab.data( 'tab' );
+          const $resultsContainer = $( `.search-results[data-content="${tabContent}"]` );
+
+          console.log( $resultsContainer );
+          // Toggle active state
+          $resultSections.removeClass( 'search-active' ).addClass( 'search-inactive' );
+          $resultsContainer.toggleClass( 'search-active' ).toggleClass( 'search-inactive' );
+
+        } else {
+          // is active tab
+
+        }
+
+      });
+
+
+    });
   }
-
-});
-
-
-//  tab click
-
-//  not active change status set results set to show
-
-
-console.log( 'tabs == ', tabs );
-
-tabs.each(( _index, tab ) => {
-  // element == this
-  const $tab = $( tab );
-
-  $tab.on( 'click', () => {
-
-    // console.log( $tab );
-    if( !$tab.parent().hasClass('active') ) {
-      // not active tab add class and remove from current
-      tabs.parents().removeClass( 'active' );
-      $tab.parent().addClass( 'active' );
-
-      // set matching content to show
-      console.log( $tab.data( 'tab' ));
-
-      const tabContent = $tab.data( 'tab' );
-      const $resultsContainer = $( `.search-results[data-content="${tabContent}"]` );
-
-      console.log( $resultsContainer );
-      $resultSections.removeClass( 'search-active' ).addClass( 'search-inactive' );
-      $resultsContainer.toggleClass( 'search-active' ).toggleClass( 'search-inactive' );
-
-    } else {
-      // is active tab
-
-    }
-
-  });
-
-
 });
