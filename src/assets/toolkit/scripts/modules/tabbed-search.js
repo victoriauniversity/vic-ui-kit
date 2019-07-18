bd0a66a5af63caf20baa40fe829e5957d3e4079a
Jake Ninness <jake.ninness@vuw.ac.nz>
2019-07-18 13:06:36 +1200

    js for toggling filter on mobile

--- src/assets/toolkit/scripts/modules/tabbed-search.js
+++ src/assets/toolkit/scripts/modules/tabbed-search.js
@@ -1,9 +1,11 @@
-$(() => {
+import enquire from 'enquire.js'
+
+$( () => {
   // Only execute if tabs exist
   if ( document.querySelectorAll( '#search-tab-js' ).length > 0 ) {
 
-    const defaultActive = $('.p-search__tabs .active a').data('tab');
-    const tabs = $('.p-search__tab a');
+    const defaultActive = $( '.p-search__tabs .active a' ).data( 'tab' );
+    const tabs = $( '.p-search__tab a' );
 
     // console.log( defaultActive );
 
@@ -12,10 +14,10 @@ $(() => {
     // console.log( $resultSections );
 
     /* Sets default vault based on default active tab */
-    $resultSections.each(( index, section ) => {
+    $resultSections.each( ( index, section ) => {
       // element == this
       const $section = $( section );
-      const sectionData = $section.data('content' );
+      const sectionData = $section.data( 'content' );
       // console.log( $section.data('content' ));
 
       // eslint-disable-next-line eqeqeq
@@ -26,7 +28,7 @@ $(() => {
         $section.addClass( 'search-inactive' );
       }
 
-    });
+    } );
 
 
     //  tab click
@@ -36,14 +38,14 @@ $(() => {
 
     // console.log( 'tabs == ', tabs );
 
-    tabs.each(( _index, tab ) => {
+    tabs.each( ( _index, tab ) => {
       // element == this
       const $tab = $( tab );
 
       $tab.on( 'click', () => {
 
         // console.log( $tab );
-        if( !$tab.parent().hasClass('active')) {
+        if ( !$tab.parent().hasClass( 'active' ) ) {
           // not active tab add class and remove from current
           tabs.parents().removeClass( 'active' );
           $tab.parent().addClass( 'active' );
@@ -65,9 +67,33 @@ $(() => {
 
         }
 
+      } );
+
+
+    } );
+
+    /* Filter toggle on mobile */
+    const TABLET_AND_SMALLER = 'screen and (max-width: 975px)';
+    let isTabletAndBelow = false;
+
+    enquire.register( TABLET_AND_SMALLER, () => {
+      isTabletAndBelow = true;
+    } );
+
+    // console.log( isTabletAndBelow );
+
+    if ( isTabletAndBelow ) {
+      $( '.filter-results-title' ).on( 'click', ( e ) => {
+        const $this = $( e );
+        // console.log( $this );
+        // Change caret
+        $( '.filter-results-title span' ).toggleClass( 'icon-caret-down' ).toggleClass( 'icon-caret-right' );
+        // show filters
+        $( '.p-search-filter-group' ).toggle( 'medium', () => {
+
+        });
       });
+    }
 
-
-    });
   }
-});
+} );
