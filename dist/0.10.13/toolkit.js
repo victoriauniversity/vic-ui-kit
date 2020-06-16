374a4fd92f699e204bb53d7ae4d90bd0143918c8
Jake Fulton <jake@jakefulton.com>
2020-06-16 12:29:25 +1200

    FIX; Improve styles for .result-list

--- dist/0.10.13/toolkit.js
+++ dist/0.10.13/toolkit.js
@@ -1,4 +1,4 @@
-/** Version: 0.10.13 | Tuesday, June 16, 2020, 11:39 AM */
+/** Version: 0.10.13 | Tuesday, June 16, 2020, 12:28 PM */
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
